c9e08d1383b06bab36129754f980d706c8a2e830
Giri1220 <165130714+Giri1220@users.noreply.github.com>
2025-01-07 19:11:05 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "dastagiri.kops.flm"
+  bucket = "dastagiri1.kops.flm"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -26,7 +26,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "us-east-1"
-bucket = "dastagiri.kops.flm"
+bucket = "dastagiri1.kops.flm"
 key = "prod/terraform.tfstate"
 }
 }
