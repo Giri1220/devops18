12f6b2788ec4e0678650cddffc18ed4abbb6b8fa
Giri1220 <165130714+Giri1220@users.noreply.github.com>
2025-01-07 18:29:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_launch_template" "web_server_as" {
     name = "myproject"
     image_id           = "ami-0ddc798b3f1a5117e"
     instance_type = "t2.micro"
-    key_name = "FLM-Keypair"
+    key_name = "FLM-KEYPAIR"
     
 }
    
